7ab4a03202d1ccefef9dec2a800103a7d8095e6c
Samia Rahmna <67817437+studycse@users.noreply.github.com>
2020-09-23 21:25:38 +0600

    Add files via upload

---
+++ reg 2/detail.php
@@ -0,0 +1,73 @@
+<?php
+	include 'dbcon.php';
+	
+
+
+	if (isset($_GET['id'])){
+
+// print_r($_POST);
+// 	exit();
+
+$id=$_GET['id'];
+
+	$detail="SELECT * FROM registertable2 where id=$id";
+	$train="SELECT * FROM training where ref_master_id=$id ";
+
+	$result = mysqli_query($connect,$detail);
+	$traini = mysqli_query($connect,$train);
+
+	 // print_r($res = mysqli_fetch_assoc($train));
+	// exit();
+
+	
+
+
+	if($res = mysqli_num_rows($result) > 0){
+		while($res = mysqli_fetch_assoc($result)){
+		echo '<h4> Id						: '.$res['id'];
+		echo '<br> name						: '.$res['username'];
+		echo '<br> email					: '.$res['email'];
+		echo '<br> address					: '.$res['address'];
+		echo '<br> mobile					: '.$res['mobile'];
+		echo '<br> Division					: '.$res['division'];
+		echo '<br> district					: '.$res['district'];
+		echo '<br> upozila					: '.$res['upozila'];
+		echo '<br> language					: '.$res['language'];
+		echo '<br> sscversity				: '.$res['sscversity'];
+		echo '<br> sscboard					: '.$res['sscboard'];
+		echo '<br> sscresult				: '.$res['sscresult'];
+		echo '<br> hscversity				: '.$res['hscversity'];
+		echo '<br> hscboard					: '.$res['hscboard'];
+		echo '<br> hscresult				: '.$res['hscresult'];
+		echo '<br> gdversity				: '.$res['gdversity'];
+		echo '<br> gdboard					: '.$res['gdboard'];
+		echo '<br> gdresult					: '.$res['gdresult'];
+		echo '<br> msversity				: '.$res['msversity'];
+		echo '<br> msboard					: '.$res['msboard'];
+		echo '<br> msresult					: '.$res['msresult'];
+		  
+
+		
+		
+	}
+	if($res = mysqli_num_rows($traini) > 0){
+			while($res = mysqli_fetch_assoc($traini)){
+			echo '<br> training_name		: '.$res['training_name'];
+		    echo '<br> organization			: '.$res['organization'];
+		    echo '<br> details				: '.$res['details'];
+			}
+				}
+
+
+
+}else{
+echo '<h2> No Result</h2>';
+
+
+}
+	
+
+	}
+
+
+?>
